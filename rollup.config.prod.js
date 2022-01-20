a87d2b82f413c5d6ddbde55c2526befa468db295
Erlend Vige <1488626+erlendev@users.noreply.github.com>
2022-01-20 12:53:12 +0100

    Update URLs pointing to asset-bucket-proxy

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -7,8 +7,8 @@ import json from "@rollup/plugin-json";
 import postcss from "rollup-plugin-postcss";
 import postcssLessLoader from "rollup-plugin-postcss-webpack-alias-less-loader";
 
-const reactUrl = "https://asset-bucket-proxy.dev.intern.nav.no/react-17.esm.js";
-const reactDomUrl = "https://asset-bucket-proxy.dev.intern.nav.no/react-dom-17.esm.js";
+const reactUrl = "https://asset-bucket-proxy.dev.intern.nav.no/asset/react/v/17/index.esm.js";
+const reactDomUrl = "https://asset-bucket-proxy.dev.intern.nav.no/asset/react-dom/v/17/index.esm.js";
 
 const imports = {
   react: reactUrl,
