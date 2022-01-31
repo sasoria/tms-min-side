f294bf250f23bb060303982dcf439359958bb41f
Erlend <erlend.edvardsen.vige@nav.no>
2022-01-31 15:34:21 +0100

    Update cdn urls to react

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import importMapPlugin from "@eik/rollup-plugin";
 
-const reactUrl = "https://asset-bucket-proxy.dev.intern.nav.no/react-17.esm.js";
+const reactUrl = "https://asset-bucket-proxy.dev.intern.nav.no/asset/react/v/17.0.2/index.esm.js";
 const reactDomUrl =
-  "https://asset-bucket-proxy.dev.intern.nav.no/react-dom-17.esm.js";
+  "https://asset-bucket-proxy.dev.intern.nav.no/asset/react-dom/v/17.0.2/index.esm.js";
 
 const imports = {
   react: reactUrl,
