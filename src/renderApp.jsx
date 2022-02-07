9dc572997d94ef7e3bde30b2b836b1dc779135ed
eivinne <eivinn_e@hotmail.com>
2022-02-07 15:55:47 +0100

    La til autentisering i dev og prod miljø, i tillegg til lokalt.

--- src/renderApp.jsx
+++ src/renderApp.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppDevelopment from './AppDevelopment';
+import Authentication from "./components/Authentication"
+import { QueryClient, QueryClientProvider } from "react-query";
 
 ReactDOM.render(
   <React.StrictMode>
-        {console.log("render")}
-    <App />
+    <QueryClientProvider client={new QueryClient()}>
+      <Authentication>
+        <AppDevelopment />
+      </Authentication>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
