a8b414c99b0b6a0fe1dcae5e245770943c87965e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-03-09 16:01:17 +0100

    Bytter til en alias-ed versjon av react og react-dom

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import importMapPlugin from "@eik/rollup-plugin";
 import { viteMockServe } from "vite-plugin-mock";
 
-const reactUrl = "https://min-side-assets.dev.intern.nav.no/asset/react/v/17.0.2/index.esm.js";
-const reactDomUrl = "https://min-side-assets.dev.intern.nav.no/asset/react-dom/v/17.0.2/index.esm.js";
+const reactUrl = "https://min-side-assets.dev.intern.nav.no/asset/react/v/17/index.esm.js";
+const reactDomUrl = "https://min-side-assets.dev.intern.nav.no/asset/react-dom/v/17/index.esm.js";
 
 export default ({ command }) => ({
   plugins: [
