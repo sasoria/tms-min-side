d01dd0e0971b22949c726e50cf58265d27e25366
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-03-25 16:59:09 +0100

    Setter opp en midlertidig sidemal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import useStore, { selectIsError } from "./store/store";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { minSideToppUrl, minSideBunnUrl } from "./api/urls";
+import "./App.css";
 
 const MinSideTopp = React.lazy(() => import(minSideToppUrl));
 const MinSideBunn = React.lazy(() => import(minSideBunnUrl));
@@ -10,17 +11,23 @@ const App = () => {
   const isError = useStore(selectIsError);
 
   return (
-    <React.Fragment>
-      {isError ? <h1>Something went wrong...</h1> : null}
-      <React.Suspense fallback="Loading...">
-        <ErrorBoundary>
-          <MinSideTopp />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <MinSideBunn />
-        </ErrorBoundary>
-      </React.Suspense>
-    </React.Fragment>
+    <div className="app">
+      <main>
+        {isError ? <h1>Something went wrong...</h1> : null}
+        <React.Suspense fallback="Loading...">
+          <section>
+            <ErrorBoundary>
+              <MinSideTopp />
+            </ErrorBoundary>
+          </section>
+          <section>
+            <ErrorBoundary>
+              <MinSideBunn />
+            </ErrorBoundary>
+          </section>
+        </React.Suspense>
+      </main>
+    </div>
   );
 };
 
