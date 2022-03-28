d074142d0c60e0b5107e79c8260720e50fd0103f
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-03-28 13:08:22 +0200

    Legger til en enkel brødsmulesti

--- server.production.js
+++ server.production.js
@@ -28,6 +28,7 @@ app.use(/^\/(?!.*dist).*$/, async (req, res) => {
       filePath: indexHtmlLocation,
       chatbot: false,
       taSurveys: false,
+      breadcrumbs: [{ url: `https://person.dev.nav.no/min-side`, title: 'Min Side' }],
     });
     res.status(200).set({ "Content-Type": "text/html" }).send(html);
   } catch (e) {
