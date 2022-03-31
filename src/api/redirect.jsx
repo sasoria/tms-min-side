fa209ec9201f3a37584bd5b8b07c3a3528bfe7b7
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-03-31 14:52:47 +0200

    Bytter over til å bruke www.dev.nav.no og www.intern.nav.no

--- src/api/redirect.jsx
+++ src/api/redirect.jsx
@@ -1,7 +1,7 @@
 import { minSideProxyUrl } from "../urls";
 
 const redirectToIdPorten = () => {
-  window.location.assign(`${minSideProxyUrl}/login?redirect_uri=https://person.dev.nav.no/minside`);
+  window.location.assign(`${minSideProxyUrl}/login?redirect_uri=https://www.dev.nav.no/minside`);
 };
 
 export default redirectToIdPorten;
