1692641b9dcfd2c36f979802f59b0df7aed3ffd8
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-04-04 15:36:25 +0200

    Bruker en intern ingress for tms-min-side-topp og tms-min-side-bunn

--- src/urls.jsx
+++ src/urls.jsx
@@ -15,13 +15,13 @@ const MIN_SIDE_PROXY_URL = {
 const MIN_SIDE_TOPP_URL = {
   local: "http://localhost:3000/tms-min-side-topp/bundle.js",
   development: "https://person.dev.nav.no/tms-min-side-topp/bundle.js",
-  production: "https://person.nav.no/tms-min-side-topp/bundle.js",
+  production: "https://person.intern.nav.no/tms-min-side-topp/bundle.js",
 };
 
 const MIN_SIDE_BUNN_URL = {
   local: "http://localhost:3000/tms-min-side-bunn/bundle.js",
   development: "https://person.dev.nav.no/tms-min-side-bunn/bundle.js",
-  production: "https://person.nav.no/tms-min-side-bunn/bundle.js",
+  production: "https://person.intern.nav.no/tms-min-side-bunn/bundle.js",
 };
 
 export const minSideToppUrl = MIN_SIDE_TOPP_URL[getEnvironment()];
