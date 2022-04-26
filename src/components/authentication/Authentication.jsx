2eed2359ce8a7d4955bc7e3017acd4e017762b54
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-04-26 13:32:06 +0200

    Bruker et ping endepunkt i dittnav-api for å sjekke om man skal redirctes til loginservice

--- src/components/authentication/Authentication.jsx
+++ src/components/authentication/Authentication.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { useQuery } from "react-query";
 import redirectToIdPorten, { redirectToLoginService } from "../../api/redirect";
-import { innloggingsstatusUrl, minSideProxyUrl } from "../../urls";
+import { authenticationUrl, minSideProxyUrl } from "../../urls";
 import { fetcher } from "../../api/api";
 import ContentLoader from "../loader/ContentLoader";
 
 const Authentication = ({ children }) => {
   const { data: status, isLoading, isError } = useQuery(`${minSideProxyUrl}/login/status`, fetcher);
-  const { data: innloggingsstatus, isLoadingInnloggingsstatus } = useQuery(innloggingsstatusUrl, fetcher);
+  const { data: legacyStatus, isLoadingLegacyStatus } = useQuery(authenticationUrl, fetcher, {
+    onError: async (error) => {
+      if (error.request.status === 401 && !isLoading) {
+        redirectToLoginService();
+      }
+    },
+  });
 
-  if (isLoading || isLoadingInnloggingsstatus) {
+  if (isLoading || isLoadingLegacyStatus) {
     return <ContentLoader size="2xlarge">...</ContentLoader>;
   }
 
   if (!status?.authenticated || isError) {
     redirectToIdPorten();
-    return null;
-  }
-
-  if (innloggingsstatus?.authenticated === false && status?.authenticated === true) {
-    redirectToLoginService();
     return null;
   }
 
