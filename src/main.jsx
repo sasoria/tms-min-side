e25cd40527533e13f54a80c77ee90b4afde5fa06
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-05-02 16:52:53 +0200

    Trekker inn dekoratøren under lokal kjøring

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Authentication from "./components/authentication/Authentication";
+import { injectDecoratorClientSide } from "@navikt/nav-dekoratoren-moduler/csr";
 import { QueryClientProvider } from "react-query";
 import { queryClient } from "./api/query";
 import "./main.css";
+
+if (process.env.NODE_ENV === "development") {
+  await injectDecoratorClientSide({
+    env: "dev",
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
