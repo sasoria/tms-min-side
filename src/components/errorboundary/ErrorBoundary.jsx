9e816360bae2a90d8db96d873b15b6db61b6120c
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-05-03 16:12:59 +0200

    Fjerner unødvendig kommentar

--- src/components/errorboundary/ErrorBoundary.jsx
+++ src/components/errorboundary/ErrorBoundary.jsx
@@ -13,7 +13,6 @@ class ErrorBoundary extends React.Component {
 
   componentDidCatch(error, errorInfo) {
     this.props.store.setIsError();
-    // logErrorToMyService(error, errorInfo);
   }
 
   render() {
