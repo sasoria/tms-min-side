640410b872de918f02a6e138ad8c03ddab3eb996
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-05-04 13:14:18 +0200

    Lager en egen komponent for min-side-varslinger

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import useStore, { selectIsError } from "./store/store";
 import Layout from "./components/layout/Layout";
 import MinSide from "./micro-frontend/MinSide";
-import ContentLoader from "./components/loader/ContentLoader";
-import ErrorBoundary from "./components/errorboundary/ErrorBoundary";
-import { minSideVarslingerUrl } from "./urls";
-
-const MinSideVarslinger = React.lazy(() => import(minSideVarslingerUrl));
+import MinSideVarslinger from "./micro-frontend/MinSideVarslinger";
 
 const App = () => {
   const isError = useStore(selectIsError);
@@ -18,16 +14,7 @@ const App = () => {
       <Layout isError={isError}>
         <Routes>
           <Route path="/minside" exact element={<MinSide />} />
-          <Route
-            path="/minside/varslinger"
-            element={
-              <React.Suspense fallback={<ContentLoader />}>
-                <ErrorBoundary>
-                  <MinSideVarslinger />
-                </ErrorBoundary>
-              </React.Suspense>
-            }
-          />
+          <Route path="/minside/varslinger" element={<MinSideVarslinger />} />
         </Routes>
       </Layout>
     </Router>
---
+++ src/micro-frontend/MinSideVarslinger.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ErrorBoundary from "../components/errorboundary/ErrorBoundary";
+import ContentLoader from "../components/loader/ContentLoader";
+import { minSideVarslingerUrl } from "../urls";
+
+const Varslinger = React.lazy(() => import(minSideVarslingerUrl));
+
+export const MinSideVarslinger = () => (
+  <React.Suspense fallback={<ContentLoader />}>
+    <ErrorBoundary>
+      <Varslinger />
+    </ErrorBoundary>
+  </React.Suspense>
+);
+
+export default MinSideVarslinger;
