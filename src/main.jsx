ee3002fb0963473cfa11e985ecdbad9e42b128f5
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-06-29 15:34:22 +0200

    Legger til sentry for å fange opp javascript feil

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
 import Authentication from "./components/authentication/Authentication";
 import { injectDecoratorClientSide } from "@navikt/nav-dekoratoren-moduler/csr";
@@ -10,6 +12,14 @@ import "./main.css";
 if (process.env.NODE_ENV === "development") {
   await injectDecoratorClientSide({
     env: "dev",
+  });
+}
+
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://06300e573bfb494a8241395dc8d43c0d@sentry.gc.nav.no/129",
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 0.1,
   });
 }
 
