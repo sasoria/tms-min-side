b8acadb5afefc09a44af45ce6c945fe81ecd4d6b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-08-25 09:51:55 +0200

    Støtter query params ved redirect etter innlogging

--- src/api/redirect.jsx
+++ src/api/redirect.jsx
@@ -1,12 +1,12 @@
 import { baseUrl, loginserviceUrl, minSideProxyUrl } from "../urls";
 
 const redirectToIdPorten = (redirectUrl) => {
-  window.location.assign(`${minSideProxyUrl}/login?redirect_uri=${redirectUrl}`);
+  window.location.assign(`${minSideProxyUrl}/login?redirect_uri=${redirectUrl}/${window.location.search}`);
 };
 
 export const redirectToLoginService = () => {
   const redirectUrl = baseUrl + window.location.pathname;
-  window.location.assign(`${loginserviceUrl}&redirect=${redirectUrl}`);
+  window.location.assign(`${loginserviceUrl}&redirect=${redirectUrl}/${window.location.search}`);
 };
 
 export default redirectToIdPorten;
