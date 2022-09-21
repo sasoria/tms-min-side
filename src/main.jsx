1946336b88ac1d0fdad01039eb0bd4060a51329b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-09-21 16:24:16 +0200

    Bytter til å bruke createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ if (getEnvironment() === "production") {
 }
 
 const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
