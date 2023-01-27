2db115e4097b0479ec6dc8dac4cc315cf6d38118
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-01-27 15:48:59 +0100

    Forenkler amplitude logging

--- src/amplitude/amplitude.js
+++ src/amplitude/amplitude.js
@@ -10,7 +10,7 @@ export const initializeAmplitude = () => {
   });
 };
 
-export function logEvent(name, label, metric) {
+export function logEvent(name, metric) {
   amplitude.getInstance().logEvent(name, {
     app: "tms-min-side",
     label: metric,
