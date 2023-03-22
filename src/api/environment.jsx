8e2091f2128fdad97161028a477c4ead2cec4322
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-03-22 15:34:08 +0100

    Bruk ny ingress standard for å sjekke environment

--- src/api/environment.jsx
+++ src/api/environment.jsx
@@ -1,5 +1,6 @@
 const isProduction = window.location.href.includes("www.intern.nav.no") || window.location.href.includes("www.nav.no");
-const isDevelopment = window.location.href.includes("www.dev.nav.no");
+const isDevelopment =
+  window.location.href.includes("www.dev.nav.no") || window.location.href.includes("www.intern.dev.nav.no");
 
 export const getEnvironment = () => {
   if (isProduction) {
