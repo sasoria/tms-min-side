29af1c9770b2dcf671cba4533ce18531151889b4
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-03-29 15:01:15 +0200

    Forneklet useLanguage koden.

--- src/hooks/useLanguage.jsx
+++ src/hooks/useLanguage.jsx
@@ -1,56 +1,35 @@
 import { onLanguageSelect, setAvailableLanguages, setParams, Params } from "@navikt/nav-dekoratoren-moduler";
 import { useEffect } from "react";
 import useStore, { selectSetLanguage, selectLanguage } from "../store/store";
-import { baseUrl } from "../urls";
+import { baseUrlWithLanguage } from "../urls";
 
-const baseUrlLocale = {
-  nb: `${baseUrl}/minside`,
-  en: `${baseUrl}/minside/en`,
-  nn: `${baseUrl}/minside/nn`,
+const setDocumentLanguage = (language) => {
+  window.sessionStorage.setItem("language", language);
+  window.dispatchEvent(new Event("language"));
+  document.documentElement.lang = language;
 };
 
-const setInitialLocale = (setLanguage, currentHref) => {
-  if (currentHref.includes(baseUrlLocale.en)) {
-    setLanguage("en");
-    setParams({ ...Params, language: "en" });
-    window.sessionStorage.setItem("language", "en");
-    window.dispatchEvent(new Event("language"));
-    document.documentElement.lang = "en";
-  } else if (currentHref.includes(baseUrlLocale.nn)) {
-    setLanguage("nn");
-    setParams({ ...Params, language: "nn" });
-    window.sessionStorage.setItem("language", "nn");
-    window.dispatchEvent(new Event("language"));
-    document.documentElement.lang = "nn";
-  } else {
-    setLanguage("nb");
-    setParams({ ...Params, language: "nb" });
-    window.sessionStorage.setItem("language", "nb");
-    window.dispatchEvent(new Event("language"));
-    document.documentElement.lang = "nb";
-  }
-};
-
-const updateState = (newLocale, currentLocale) => {
-  const replacementUrl = window.location.href.replace(baseUrlLocale[currentLocale], baseUrlLocale[newLocale]);
-
+const updateState = (newLanguage, currentLanguage) => {
+  const replacementUrl = window.location.href.replace(
+    baseUrlWithLanguage[currentLanguage],
+    baseUrlWithLanguage[newLanguage]
+  );
   window.history.replaceState(window.history.state, "", replacementUrl);
 };
 
 export const useLanguage = () => {
   const setLanguage = useStore(selectSetLanguage);
-  const currentLocale = useStore(selectLanguage);
+  const currentLanguage = useStore(selectLanguage);
 
   onLanguageSelect((language) => {
+    updateState(language.locale, currentLanguage);
     setLanguage(language.locale);
-    updateState(language.locale, currentLocale);
-    window.sessionStorage.setItem("language", language.locale);
-    window.dispatchEvent(new Event("language"));
-    document.documentElement.lang = language.locale;
+    setDocumentLanguage(language.locale);
   });
 
   useEffect(() => {
-    setInitialLocale(setLanguage, window.location.href.toLocaleLowerCase());
+    setParams({ ...Params, language: currentLanguage });
+    setDocumentLanguage(currentLanguage);
     setAvailableLanguages([
       {
         locale: "nb",
