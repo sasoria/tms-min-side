f7767554fa97c1f434f1b81b1b7fd6d9e14ec4eb
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-04-18 18:09:48 +0200

    Forenkle vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import importMapPlugin from "@eik/rollup-plugin";
+import { rollupImportMapPlugin } from "rollup-plugin-import-map";
 import { viteMockServe } from "vite-plugin-mock";
 import { terser } from "rollup-plugin-terser";
-
-const reactUrl = "https://www.nav.no/tms-min-side-assets/react/18/esm/index.js";
-const reactDomUrl = "https://www.nav.no/tms-min-side-assets/react-dom/18/esm/index.js";
+import importmap from "./importmap.json";
 
 export default ({ command }) => ({
   plugins: [
@@ -15,16 +13,7 @@ export default ({ command }) => ({
       localEnabled: command === "serve",
     }),
     {
-      ...importMapPlugin({
-        maps: [
-          {
-            imports: {
-              react: reactUrl,
-              "react-dom": reactDomUrl,
-            },
-          },
-        ],
-      }),
+      ...rollupImportMapPlugin([importmap]),
       enforce: "pre",
       apply: "build",
     },
