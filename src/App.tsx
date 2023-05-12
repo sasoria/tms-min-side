17367f159abe5c1802373ceceadf1f8ed7fe0ce3
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2023-05-12 11:08:24 +0200

    Fjern route for varslinger

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,8 @@ const App = () => {
           return (
             <>
               <Route path={basePath} element={<MinSide />} />
-              <Route path={`${basePath}/varslinger`} element={<TidligereVarsler />} />
-              <Route path={`${basePath}/tidligere-varsler`} element={<TidligereVarsler />} />
               <Route path={`${basePath}/varsler`} element={<Varsler />} />
+              <Route path={`${basePath}/tidligere-varsler`} element={<TidligereVarsler />} />
               <Route path={`${basePath}/arbeidssoker`} element={<Arbeidssoker />} />
               <Route path={`${basePath}/utkast`} element={<Utkast />} />
             </>
